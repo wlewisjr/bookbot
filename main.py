3a205aa3117289d8eb297bd1dae27ac2a7928351
wlewisjr <wsljr1986@gmail.com>
2025-02-22 12:10:38 -0500

    add word count implementation to main.py

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-print("greetings boots")
+def get_book_text(file_path):
+    with open(file_path) as file:
+        file_contents = file.read()
+    return file_contents
+
+def get_word_count(text):
+    words = text.split()
+    return len(words)
+
+def main():
+    book = "books/frankenstein.txt"
+    text = get_book_text(book)
+    word_count = get_word_count(text)
+    print(f"{word_count} words found in the document")
+
+main()
